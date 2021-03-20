e24cf52a6654754c9ac58872ce909d979de7effa
Chris Hemmings <cwhemmings@gmail.com>
2021-03-20 22:08:33 +0000

    dsasda

--- src/pdfium_win_x64.cxx
+++ src/pdfium_win_x64.cxx
@@ -8382,6 +8382,37 @@ fail:
   SWIGV8_RETURN(SWIGV8_UNDEFINED());
 }
 
+static SwigV8ReturnValue _wrap_CreateEnhMetaFileWNull(const SwigV8Arguments &args)
+{
+  SWIGV8_HANDLESCOPE();
+
+  SWIGV8_VALUE jsresult;
+  HDC arg1;
+  LPCWSTR arg2;
+  RECT *arg3 = (RECT *)0;
+  LPCWSTR arg4;
+  void *argp1;
+  int res1 = 0;
+  void *argp2;
+  int res2 = 0;
+  void *argp3 = 0;
+  int res3 = 0;
+  void *argp4;
+  int res4 = 0;
+  HDC result;
+
+  if (args.Length() != 4)
+    SWIG_exception_fail(SWIG_ERROR, "Illegal number of arguments for _wrap_CreateEnhMetaFileW.");
+
+  result = CreateEnhMetaFileW(NULL, NULL, NULL, NULL);
+  jsresult = SWIG_NewPointerObj((new HDC(static_cast<const HDC &>(result))), SWIGTYPE_p_HDC, SWIG_POINTER_OWN | 0);
+
+  SWIGV8_RETURN(jsresult);
+
+  goto fail;
+fail:
+  SWIGV8_RETURN(SWIGV8_UNDEFINED());
+}
 
 /* -------- TYPE CONVERSION AND EQUIVALENCE RULES (BEGIN) -------- */
 
@@ -9220,7 +9251,7 @@ SWIGV8_AddStaticFunction(exports_obj, "Rectangle", _wrap_Rectangle, context);
 SWIGV8_AddStaticFunction(exports_obj, "CreateEnhMetaFileW", _wrap_CreateEnhMetaFileW, context);
 SWIGV8_AddStaticFunction(exports_obj, "GetEnhMetaFileBits", _wrap_GetEnhMetaFileBits, context);
 SWIGV8_AddStaticFunction(exports_obj, "CloseEnhMetaFile", _wrap_CloseEnhMetaFile, context);
-
+SWIGV8_AddStaticFunction(exports_obj, "CreateEnhMetaFileWNull", _wrap_CreateEnhMetaFileWNull, context);
 
   /* register classes */
   #if (V8_MAJOR_VERSION-0) < 5
@@ -9281,37 +9312,3 @@ NODE_MODULE(pdfium, pdfium_initialize)
 NODE_MODULE_CONTEXT_AWARE(pdfium, pdfium_initialize)
 #endif
 #endif
-
-static SwigV8ReturnValue _wrap_CreateEnhMetaFileWNull(const SwigV8Arguments &args)
-{
-    SWIGV8_HANDLESCOPE();
-
-    SWIGV8_VALUE jsresult;
-    HDC arg1;
-    LPCWSTR arg2;
-    RECT *arg3 = (RECT *)0;
-    LPCWSTR arg4;
-    void *argp1;
-    int res1 = 0;
-    void *argp2;
-    int res2 = 0;
-    void *argp3 = 0;
-    int res3 = 0;
-    void *argp4;
-    int res4 = 0;
-    HDC result;
-
-    if (args.Length() != 4)
-        SWIG_exception_fail(SWIG_ERROR, "Illegal number of arguments for _wrap_CreateEnhMetaFileW.");
-
-    result = CreateEnhMetaFileW(NULL, NULL, NULL, NULL);
-    jsresult = SWIG_NewPointerObj((new HDC(static_cast<const HDC &>(result))), SWIGTYPE_p_HDC, SWIG_POINTER_OWN | 0);
-
-    SWIGV8_RETURN(jsresult);
-
-    goto fail;
-fail:
-    SWIGV8_RETURN(SWIGV8_UNDEFINED());
-}
-
-SWIGV8_AddStaticFunction(exports_obj, "CreateEnhMetaFileWNull", _wrap_CreateEnhMetaFileWNull, context);
